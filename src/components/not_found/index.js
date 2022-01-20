981677e90b2c39daade0a5484d91121f96d855ae
Rodrigo <rodrigo.cdpadua@gmail.com>
2022-01-20 09:06:11 -0300

    add page not found

---
+++ src/components/not_found/index.js
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const NotFound = () => {
+    return(
+        <div className='not-found-container'>
+            <h1>PAGE NOT FOUND</h1>
+            <p>Please use the navigation bar</p>
+        </div>
+    );
+}
+
+export default NotFound
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import Header from './components/header';
 import Connection from './pages/connection';
 import AddSchools from './pages/add_schools';
 import AboutMe from './pages/about_me';
+//import ListSchoolsApi from './pages/list_schools_api';
+import NotFound from './components/not_found';
 import { AuthProvider, AuthContext } from './auth';
 
 const AppRoutes = () => {
@@ -29,9 +31,9 @@ const AppRoutes = () => {
                 <Routes>
                     <Route path='/' element={<LoggedOut><Connection /></LoggedOut>}/>
                     <Route path='add-schools' element={<Private><AddSchools /></Private>} />
-                    <Route path='list-schools' element={<Private><h1>Schools List</h1></Private>}/>
+                    <Route path='list-schools' element={<Private><h1>Shcools List</h1></Private>}/>
                     <Route path='about-me' element={<AboutMe />} />
-                    <Route path='*' element={<h1>Not Found</h1>} />
+                    <Route path='*' element={<NotFound />} />
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
