c5472ca2a5804eb596a2be078d497edbf3434d04
Rodrigo <rodrigo.cdpadua@gmail.com>
2022-01-20 23:50:58 -0300

    delete added schools

--- src/pages/add_schools/add_school_list.js
+++ src/pages/add_schools/add_school_list.js
@@ -4,42 +4,59 @@ const AddSchoolList = (props) => {
     const [listSchools, setListSchools] = useState([]);
 
     useEffect(() => {
-        console.log("UseEffect")
+        updateListSchools()
+    },[props.listSchoolsChange])
 
+    const updateListSchools = () => {
         fetch('http://localhost:5000/schools')
         .then(res => res.json())
         .then(
             schools => { setListSchools(schools) }
         );
-    },[props.listSchoolsChange])
+    }
+
+    const deleteSchool = (e) => {
+        const answer = window.confirm('Are you sure to delete this school?')
+
+        if(answer){
+            fetch('http://localhost:5000/schools/' + e.target.value, {
+                method: 'DELETE'
+            })
+            .then(updateListSchools())
+        }
+    }
 
     return(
         <div className="add-school-list-container">
-            <div className="add-school-list-card">
-                <h2>Added Schools</h2>
-                <table className='table-list-school'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Director</th>
-                            <th>Localization</th>
-                            <th>Shift</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            listSchools.map((school, key) => 
-                                <tr key={key}>
-                                    <td>{school.schoolName}</td>
-                                    <td>{school.director}</td>
-                                    <td>{school.localization}</td>
-                                    <td>{school.shift.join(' / ')}</td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
+            {listSchools.length !== 0 &&
+                <div className="add-school-list-card">
+                    <h2>Added Schools</h2>
+                    <table className='table-list-school'>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Director</th>
+                                <th>Localization</th>
+                                <th>Shift</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                listSchools.map((school, key) => 
+                                    <tr key={key}>
+                                        <td>{school.schoolName}</td>
+                                        <td>{school.director}</td>
+                                        <td>{school.localization}</td>
+                                        <td>{school.shift.join(' / ')}</td>
+                                        <td id="td-delete"><button value={school.id} className="delete-school-button" onClick={deleteSchool}>X</button></td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            }
         </div>
     );
 }
